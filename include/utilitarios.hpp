70a87f3820b61c1f3d475ad42a10058e8a8ec880
samarasou <samavic86@gmail.com>
2025-01-17 13:12:44 -0300

    Update utilitarios.hpp

--- include/utilitarios.hpp
+++ include/utilitarios.hpp
@@ -1,1 +1,15 @@
+#ifndef UTILITARIOS_HPP
+#define UTILITARIOS_HPP
 
+#include <string>
+#include <vector>
+#include <ctime>
+
+bool validarTelefone (const std::string& telefone);
+bool validarEmail (const std::string& email);
+void salvarDadosEmArquivo (const std::string& nomeArquivo, const std::vector<std::string>& dados);
+std::vector<std::string> lerDadosArquivo (const std::string& nomeArquivo);
+int calcularDiasDeDiferenca (const std::tm& data1, const std::tm& data2);
+double calcularMultaPorAtraso (int diasAtraso, double valorPorDia);
+
+#endif // ULTILITARIOS_HPP
