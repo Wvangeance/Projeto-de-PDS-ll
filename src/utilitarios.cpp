61841d63844870d9d4fc51f6d0c866d1d101fe96
samarasou <samavic86@gmail.com>
2025-01-19 22:00:35 -0300

    Update utilitarios.cpp

--- src/utilitarios.cpp
+++ src/utilitarios.cpp
@@ -16,7 +16,7 @@ bool validarEmail (const std::string& email) {
 return std::regex_match (email, emailRegex);
 }
 
-Função para salvar dados em um arquivo
+//Função para salvar dados em um arquivo
 
 void salvarDadosEmArquivo (const std::string& nomeArquivo, const std::vector<std::string>& dados) {
   std::ofstream arquivo (nomeArquivo);
@@ -29,3 +29,31 @@ arquivo.close ();
 }
 
 //Função para ler dados de um arquivo
+
+std::vector<std::string> lerDadosDeArquivo (const std::string& nomeArquivo) {
+  std::vector<strd::string> dados;
+std::ifstream arquivo (nomrArquivo);
+std::string linha;
+if (arquivo.is_open()) {
+dados.push_back (linha);
+}
+arquivo.close();
+}
+return dados;
+}
+
+//Função para calcular diferença de dias entre duas datas
+
+int calcularDiasDeDiferenca (const std::tm& data1, const std::tm& data2) {
+  std::time_t t1 = std::mktime(const_cast<std::tm*>(&data1));
+  std::time_t t2 = std::mktime(const_cast<std::tm*>(&data2));
+return std::difftime (t2,t1)/(60*60*24);
+}
+
+//Função para calcular multa por atraso
+
+double calcularMultaPorAtraso (int diasAtraso, double valorPorDia) {
+  return diasAtraso*valorPorDia;
+}
+
+  
