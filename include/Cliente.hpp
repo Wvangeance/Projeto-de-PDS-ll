d9aa2bf428124152320d1d5c9e4c0f0b41387067
Vinícius Silva Santos <viniciussilvasantos04@gmail.com>
2025-01-21 23:21:51 -0300

    Update Cliente.hpp

--- include/Cliente.hpp
+++ include/Cliente.hpp
@@ -1,23 +1,24 @@
-#ifndef Cadastro_Cliente_HPP
-#define Cadastro_Cliente_HPP
+#ifndef CLIENTE_HPP
+#define CLIENTE_HPP
 
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <sstream>
+#include <limits>
 
 class Pessoa {
 private:
     std::string nome;
     std::string cpf;
+    bool permitidoLocacao;
 
 public:
-
     Pessoa();
-  
- 
     void coletarDados();
     void cadastrarNoArquivo();
-    void ListarArquivo();
+    void listarArquivo();
+    void editarPermissaoLocacao();
 };
 
-#endif 
+#endif
