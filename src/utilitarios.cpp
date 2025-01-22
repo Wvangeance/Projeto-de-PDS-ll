b3381737f322a8c03e708c57fcdf8e4e19905b6a
Ryan Avila <ryanavilasobrinhogomes@gmail.com>
2025-01-22 11:38:26 -0300

    Update utilitarios.cpp

--- src/utilitarios.cpp
+++ src/utilitarios.cpp
@@ -5,24 +5,24 @@
 //Função para validar formato de telefone
 bool validarTelefone (const std::string& telefone) {
   std::regex telefoneRegex ("\\d{3}-\\d{3}-\\d{4}");
-return std::regex_match (telefone, telefoneRegex);
+  return std::regex_match (telefone, telefoneRegex);
 }
 
 //Função para validar formato de email
 bool validarEmail (const std::string& email) {
   std::regex emailRegex ("(\\x+)(\\.|_)?(\\w*)@(\\w+)(\\.(\\w+))+");
-return std::regex_match (email, emailRegex);
+  return std::regex_match (email, emailRegex);
 }
 
 //Função para salvar dados em um arquivo
 void salvarDadosEmArquivo (const std::string& nomeArquivo, const std::vector<std::string>& dados) {
   std::ofstream arquivo (nomeArquivo);
-if (arquivo.is_open()) {
-for (const auto& linha : dados) {
-arquivo << linha << "\n";
-}
-arquivo.close ();
-}
+  if (arquivo.is_open()) {
+    for (const auto& linha : dados) {
+      arquivo << linha << "\n";
+    }
+    arquivo.close ();
+  }
 }
 
 //Função para ler dados de um arquivo
@@ -31,7 +31,9 @@ std::vector<std::string> lerDadosDeArquivo (const std::string& nomeArquivo) {
 std::ifstream arquivo (nomrArquivo);
 std::string linha;
 if (arquivo.is_open()) {
-dados.push_back (linha);
+  while(std::getline(arquivo, linha)){
+    dados.push_back (linha);
+  }
 }
 arquivo.close();
 }
@@ -47,5 +49,5 @@ return std::difftime (t2,t1)/(60*60*24);
 
 //Função para calcular multa por atraso
 double calcularMultaPorAtraso (int diasAtraso, double valorPorDia) {
-  return diasAtraso*valorPorDia;
+  return diasAtraso * valorPorDia;
 }
