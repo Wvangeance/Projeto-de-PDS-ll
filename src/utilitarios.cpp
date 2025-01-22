3c75535e9400b7e500d378a316b77566645cc1c5
Ryan Avila <ryanavilasobrinhogomes@gmail.com>
2025-01-22 11:23:28 -0300

    Update utilitarios.cpp

--- src/utilitarios.cpp
+++ src/utilitarios.cpp
@@ -3,21 +3,18 @@
 #include <fstream>
 
 //Função para validar formato de telefone
-
 bool validarTelefone (const std::string& telefone) {
   std::regex telefoneRegex ("\\d{3}-\\d{3}-\\d{4}");
 return std::regex_match (telefone, telefoneRegex);
 }
 
 //Função para validar formato de email
-
 bool validarEmail (const std::string& email) {
   std::regex emailRegex ("(\\x+)(\\.|_)?(\\w*)@(\\w+)(\\.(\\w+))+");
 return std::regex_match (email, emailRegex);
 }
 
 //Função para salvar dados em um arquivo
-
 void salvarDadosEmArquivo (const std::string& nomeArquivo, const std::vector<std::string>& dados) {
   std::ofstream arquivo (nomeArquivo);
 if (arquivo.is_open()) {
@@ -29,9 +26,8 @@ arquivo.close ();
 }
 
 //Função para ler dados de um arquivo
-
 std::vector<std::string> lerDadosDeArquivo (const std::string& nomeArquivo) {
-  std::vector<strd::string> dados;
+  std::vector<std::string> dados;
 std::ifstream arquivo (nomrArquivo);
 std::string linha;
 if (arquivo.is_open()) {
@@ -43,7 +39,6 @@ return dados;
 }
 
 //Função para calcular diferença de dias entre duas datas
-
 int calcularDiasDeDiferenca (const std::tm& data1, const std::tm& data2) {
   std::time_t t1 = std::mktime(const_cast<std::tm*>(&data1));
   std::time_t t2 = std::mktime(const_cast<std::tm*>(&data2));
@@ -51,9 +46,6 @@ return std::difftime (t2,t1)/(60*60*24);
 }
 
 //Função para calcular multa por atraso
-
 double calcularMultaPorAtraso (int diasAtraso, double valorPorDia) {
   return diasAtraso*valorPorDia;
 }
-
-  
