6a4f942b2e677ec3627bf0bca0930c511e96320b
samarasou <samavic86@gmail.com>
2025-01-28 16:52:22 -0300

    Update Historico.cpp

--- src/Historico.cpp
+++ src/Historico.cpp
@@ -10,4 +10,32 @@ void Historico::listarAtividades() const {
   std::cout<<"Histórico de Atividades:\n";
 for (const auto& atividade : atividades) {
 std::cout<<"- "<<atividade<<"\n";
+}
+}
 
+\\Salvar histórico
+void Historico::salvarHistoricoEmArquivo(const std::string& nomeArquivo) const {
+  std::ofstream arquivo(nomeArquivo);
+if (arquivo.is_open()) {
+for (const auto& atividade : atividades) {
+arquivo << atividade << "\n";
+}
+arquivo.close();
+} else {
+std::cerr << "Erro ao abrir arquivo para salvar o histórico.\n";
+}
+}
+
+//Carregar histórico 
+void Historico::carregarHistoricoDeArquivo(const std::string& nomeArquivo) {
+  std::ifstream arquivo(nomeArquivo);
+std::string linha;
+if (arquivo.is_open()) {
+while (std::getline(arquivo, linha)) {
+atividades.push_back(linha);
+}
+arquivo.close();
+} else {
+std::cerr << "Erro ao abrir o arquivo para carregar histórico.\n";
+}
+}
