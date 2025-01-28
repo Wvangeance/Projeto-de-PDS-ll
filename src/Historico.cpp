e658e88b64ff6aaec711c278349bcb4c8f30cec0
samarasou <samavic86@gmail.com>
2025-01-28 16:21:45 -0300

    Update Historico.cpp

--- src/Historico.cpp
+++ src/Historico.cpp
@@ -1,1 +1,13 @@
+#include "Historico.hpp"
+
+//Nova atividade
+vpid Historico::adicionarAtividade(const std::string& atividade) {
+  atividade.push_back(atividade);
+}
+
+//Listar atividades
+void Historico::listarAtividades() const {
+  std::cout<<"Histórico de Atividades:\n";
+for (const auto& atividade : atividades) {
+std::cout<<"- "<<atividade<<"\n";
 
