9e487e02a0268521f6be6a9fed3e0448b0921c49
William Dhomini <williamdhomini7@gmail.com>
2025-01-27 22:47:08 -0300

    Update Pagamento.hpp

--- include/Pagamento.hpp
+++ include/Pagamento.hpp
@@ -1,62 +1,52 @@
-#ifndef LOCADORA_H
-#define LOCADORA_H
+#ifndef PAGAMENTO_HPP
+#define PAGAMENTO_HPP
 
 #include <iostream>
 #include <memory>
 #include <limits>
+#include "Locações.hpp"
 
 // Classe base abstrata Pagamento
 class Pagamento {
 public:
-    virtual void realizarPagamento(double valor, bool &pagamentoRealizado) = 0;
+    virtual void realizarPagamento(const Locacao& locacao, bool& pagamentoRealizado) = 0;
     virtual ~Pagamento() {}
 };
 
 // Classe PagamentoDinheiro
 class PagamentoDinheiro : public Pagamento {
 public:
-    void realizarPagamento(double valor, bool &pagamentoRealizado) override;
+    void realizarPagamento(const Locacao& locacao, bool& pagamentoRealizado) override;
 };
 
 // Classe PagamentoCartaoCredito
 class PagamentoCartaoCredito : public Pagamento {
 public:
-    void realizarPagamento(double valor, bool &pagamentoRealizado) override;
+    void realizarPagamento(const Locacao& locacao, bool& pagamentoRealizado) override;
 };
 
 // Classe PagamentoCartaoDebito
 class PagamentoCartaoDebito : public Pagamento {
 public:
-    void realizarPagamento(double valor, bool &pagamentoRealizado) override;
+    void realizarPagamento(const Locacao& locacao, bool& pagamentoRealizado) override;
 };
 
 // Classe PagamentoPix
 class PagamentoPix : public Pagamento {
 public:
-    void realizarPagamento(double valor, bool &pagamentoRealizado) override;
+    void realizarPagamento(const Locacao& locacao, bool& pagamentoRealizado) override;
 };
 
 // Classe PagamentoEscambo
 class PagamentoEscambo : public Pagamento {
 public:
-    void realizarPagamento(double valor, bool &pagamentoRealizado) override;
+    void realizarPagamento(const Locacao& locacao, bool& pagamentoRealizado) override;
 };
 
-// Classe para calculo de multas por atraso
+// Classe para cálculo de multas por atraso
 class Multa {
 public:
-    static double calcularMulta(double valorEmprestimo, int diasAtraso, int diasPermitidos);
+    static double calcularMulta(const Locacao& locacao, int diasPermitidos);
 };
 
-// Classe Locadora
-class Locadora {
-private:
-    std::unique_ptr<Pagamento> metodoPagamento;
-
-public:
-    Locadora(std::unique_ptr<Pagamento> metodo);
-    void processarPagamento(double valor, bool &pagamentoRealizado);
-    void alterarMetodoPagamento(std::unique_ptr<Pagamento> novoMetodo);
-};
-
-#endif // LOCADORA_H
+#endif // PAGAMENTO_HPP
