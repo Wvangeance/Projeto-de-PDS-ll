42e34c8d90022a96b91642cb87d17874eb1f67a6
William Dhomini <williamdhomini7@gmail.com>
2025-01-29 12:53:52 -0300

    Update Historico.hpp

--- include/Historico.hpp
+++ include/Historico.hpp
@@ -2,7 +2,7 @@
 #define HISTORICO_HPP
 
 #include "Cliente.hpp"
-#include "Locações.hpp"
+#include "Locacoes.hpp"
 #include "Pagamento.hpp"
 #include "Filmes.hpp"
 #include <vector>
@@ -10,12 +10,14 @@
 #include <string>
 
 class Historico {
+public:
+    std::map<int, Pessoa> clientes; // Dados de interação dos clientes, como locações e devoluções
+    std::map<int, int> clientesFrequentes; // Contagem de locações por cliente
+
 private:
     std::vector<Locacao> locacoes; // Registro de todas as locações realizadas
     std::vector<Pagamento> pagamentos; // Histórico de pagamentos efetuados e pendências
-    std::map<int, Pessoa> clientes; // Dados de interação dos clientes, como locações e devoluções
     std::map<std::string, int> estatisticasFilmes; // Contagem de filmes mais alugados
-    std::map<int, int> clientesFrequentes; // Contagem de locações por cliente
 
     void atualizarEstatisticas(const Locacao& locacao); // Atualiza estatísticas ao registrar uma locação
     void registrarPagamento(const Pagamento& pagamento); // Adiciona um novo pagamento no histórico
